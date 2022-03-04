d3b781a13d560f9731ddb104e2e51b754f2957ea
Boody <cfbuddy124@gmail.com>
2022-03-04 22:31:42 +0200

    fix singular queries

--- src/resolvers/query.js
+++ src/resolvers/query.js
@@ -4,10 +4,9 @@ const query = {
   products: () => prisma.product.findMany(),
   users: () => prisma.user.findMany(),
   sellers: () => prisma.seller.findMany(),
-  user: (_parent, args) =>
-    prisma.product.findUnique({ where: { id: args.id } }),
+  user: (_parent, args) => prisma.user.findUnique({ where: { id: args.id } }),
   product: (_parent, args) =>
-    prisma.seller.findUnique({ where: { id: args.id } }),
+    prisma.product.findUnique({ where: { id: args.id } }),
   seller: (_parent, args) =>
     prisma.seller.findUnique({ where: { id: args.id } }),
 };
