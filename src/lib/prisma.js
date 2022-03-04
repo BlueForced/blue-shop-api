acfa2c341d8be4767f2b8585f304b6605444895c
Boody <cfbuddy124@gmail.com>
2022-03-04 23:06:17 +0200

    add soft delete middleware

--- src/lib/prisma.js
+++ src/lib/prisma.js
@@ -2,4 +2,33 @@ import Prisma from '@prisma/client';
 const { PrismaClient } = Prisma;
 const prisma = new PrismaClient();
 
+prisma.$use(async (params, next) => {
+  if (params.action === 'delete') {
+    params.action = 'update';
+    params.args.data = { isDeleted: true };
+  } else if (params.action === 'deleteMany') {
+    params.action = 'updateMany';
+    if (params.args.data) {
+      params.args.data.isDeleted = true;
+    } else {
+      params.args.data = { isDeleted: true };
+    }
+  } else if (params.action === 'findFirst') {
+    params.args.where.isDeleted = false;
+  } else if (params.action === 'findUnique') {
+    params.action = 'findFirst';
+    params.args.where.isDeleted = false;
+  } else if (params.action === 'findMany') {
+    if (!params.args) {
+      params.args = { where: { isDeleted: false } };
+    } else if (!params.args.where) {
+      params.args.where = { isDeleted: false };
+    } else {
+      params.args.where.isDeleted = false;
+    }
+  }
+
+  return next(params);
+});
+
 export default prisma;
